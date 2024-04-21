ef0b31adb019643899b5125bad47d2caa94ea01f
Siphamandla <104821114+marenenes@users.noreply.github.com>
2024-04-21 12:51:16 +0200

    Update TestRunner.java

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/Features",
                  glue = "StepDefinitions",
-                 tags="@E2E",
                  plugin= {"pretty"
                          ,"html:target/report/report.html"
 
